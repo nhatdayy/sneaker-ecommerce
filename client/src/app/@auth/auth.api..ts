ead81736afb2b018d089da4454877e49f2dc7b2a
MNhatdayy <nhatnm2003@gmail.com>
2025-04-19 23:14:40 +0700

    update-base-reactJS

--- client/src/app/@auth/auth.api..ts
+++
@@ -1,7 +0,0 @@
-export const AuthApi = {
-  login: 'Auth/login',
-  register: 'Auth/register',
-  changePassword: 'Auth/change-password',
-  restorePassword: 'Auth/restore-password',
-  refreshToken: 'Auth/refresh-token',
-};
--- client/src/app/core/constants/secret.ts
+++
@@ -1,1 +0,0 @@
-export const secretKeyToken = 'NMN30112003';
